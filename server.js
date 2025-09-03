659f532cd499fa01c2d3092c06d575ae0707bb71
Athul-042 <athulpalanichamy076@gmail.com>
2025-09-03 23:46:42 +0530

    added

--- server.js
+++ server.js
@@ -7,9 +7,12 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb+srv://Athul:Athul123@cluster0.iusbbwz.mongodb.net/labdb?retryWrites=true&w=majority&appName=Cluster0')
-    .then(() => console.log("MongoDB Atlas Connected"))
-    .catch(err => console.log(err));
+mongoose.connect('mongodb+srv://Athul:Athul1234@cluster0.mongodb.net/labdb?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB Atlas Connected"))
+.catch(err => console.log(err));
 
 const itemRoutes = require('./routes/itemRoutes');
 app.use('/api/items', itemRoutes);
